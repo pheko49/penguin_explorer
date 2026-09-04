136fe993013ee20606ae32d3f2702a2182dc420e
pheko49 <phekofm49.pm@gmail.com>
2026-09-05 00:38:33 +0200

    Configure Penguin admin

--- penguins/admin.py
+++ penguins/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Penguin
 
 # Register your models here.
+
+@admin.register(Penguin)
+class PenguinAdmin(admin.ModelAdmin):
+    list_display = ("id", "species", "island", "sex", "body_mass_g")
+    list_filter = ("species", "island", "sex")
+    search_fields = ("species", "island")
