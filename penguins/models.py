32d91ee662abf44809cdca41fceb1a4e496e046d
pheko49 <phekofm49.pm@gmail.com>
2026-09-05 00:25:03 +0200

    Add Penguin model and initial migration

--- penguins/models.py
+++ penguins/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Penguin(models.Model):
+    species = models.CharField(max_length=50)
+    island = models.CharField(max_length=50)
+    bill_length_mm = models.FloatField()
+    bill_depth_mm = models.FloatField()
+    flipper_length_mm = models.PositiveIntegerField()
+    body_mass_g = models.PositiveIntegerField()
+    sex = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return f'{self.species} - {self.island}'
